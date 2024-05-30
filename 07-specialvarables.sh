03cf4f634172bfa92da4d9961db7580df601a7e0
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 11:57:00 +0530

    shellscript

---
+++ 07-specialvarables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All varables: $@"
+echo "Number of varables: $#"
+echo "Script Name: $0"
+echo "Current Working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname:$HOSTNAME"
+echo "Process ID of the current shell script: $$"
+
