8a21ba45eb2ca39b7c4d8de270f7c240eb31e006
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 07:35:08 +0530

    shellscript

--- 02-varables.sh
+++ 02-varables.sh
@@ -1,8 +1,8 @@
 
-var1=Ramesh
-var2=Suresh
+var1=Hemanth
+var2=Rakesh
 
-echo "$var1":Hello $var2,how are you"
-echo "$var2:Hi $var1, Iam fine How are you?"
-echo "$var1":Iam fine how is your work"
-echo "$var2":not bad iam thinking to upgrader"
+echo "$var1:Hello $var2,how are you"
+echo "$var2:Hi $var1,Iam fine How are you?"
+echo "$var1:Iam fine how is your work"
+echo "$var2:not bad iam thinking to upgrader Devops"
