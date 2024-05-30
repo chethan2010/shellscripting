7e96e58328e95013d02d9b585926b0e9b4511f60
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 10:41:42 +0530

    shellscript

---
+++ 03-varables.sh
@@ -0,0 +1,7 @@
+var1=$var1
+var2=$var2
+
+echo "$var1:Hello $var2,how are you"
+echo "$var2:Hi $var1,Iam fine How are you?"
+echo "$var1:Iam fine how is your work"
+echo "$var2:not bad iam thinking to upgrader Devops"
