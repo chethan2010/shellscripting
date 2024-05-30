acafd75dd9313283ae580d7d761f864200bce9eb
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 20:11:56 +0530

    shell-script

--- 08-installingSql.sh
+++ 08-installingSql.sh
@@ -5,10 +5,12 @@ if [userid -ne 0]
 then
 
 echo "Please run the script with root access"
-
+exit 1
 else
 
 echo "you are super user."
 
 fi
 
+#dnf install mysql -y
+#validate $? "Installing Mysql"
