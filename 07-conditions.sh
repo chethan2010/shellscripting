95d36f215930ffdcdd0534e163bc9695e8691360
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 12:19:24 +0530

    shellscript

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 i=$100
-if [$i-gt 10] 
-then echo "given numebr  is greter than 10"
-else echo "given is less than 10"
+if [$i -gt 10] 
+then 
+echo "given $i  is greter than 10"
+else 
+echo "given $i is less than 10"
 fi
