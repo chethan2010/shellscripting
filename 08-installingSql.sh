d835ab25a0f6fa5337fc867cd13afcd7918d664b
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 20:29:04 +0530

    shell-script

--- 08-installingSql.sh
+++ 08-installingSql.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 USERID= $(id -u)
-validate() {
+val
 echo "Exit status:"
 echo "What are you doing
-}
-
-# if [ $USERID -ne 0]
-# then
-# echo "Please run the script with root access"
-# exit 1
-# else
-# echo "you are super user."
-# fi
-
-# dnf install mysql -y
-
-# validate $? (Installing MYSQL)
-
-# if [ $USERID -ne 0]
-# then
-# echo "Please run the script with root access"
-# exit 1
-# else
-# echo "you are super user."
-# fi
-
-# #dnf install mysql -y
-# #validate $? "Installing Mysql"
