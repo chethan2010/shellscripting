2866a16cb93283e85ec9696b0bbcb2b7cea96b38
Hemanth <hemanthkumarsaketi@gmail.com>
2024-05-30 12:31:31 +0530

    shellscript

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,10 +1,10 @@
 #!bin/bash
-Number=$100
-if [ $Number1 -gt 10] 
+Number=$1
+if [$Number1 > 10] 
 then 
 echo "given $Number1  is greter than 10"
 else 
 echo "given $Number1 is less than 10"
 fi
 
-# -gt ,-eq ,lt
+# -gt ,-eq ,-lt
